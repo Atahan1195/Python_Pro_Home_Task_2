c63a64bbbaf126e56f9abb2cd08d80c0f443cbb0
Atahan <atahan11951195@gmail.com>
2023-12-21 10:31:44 +0400

    Fix

--- main.py
+++ main.py
@@ -7,86 +7,86 @@
 # Корзина может содержать несколько продуктов определенного количества. Реализуйте метод вычисления стоимости корзины.
 # Во всех классах должен быть определен метод str.
 
-# class Product:
-#     """Class for product
-#     Attributes:
-#         name (str): name of product
-#         price (int): price of product
-#         description (str): description of product
-#     """
-#
-#     def __init__(self, name, price, description):
-#         self.name = name
-#         self.price = price
-#         self.description = description
-#
-#     def __str__(self):
-#         return f"Product: {self.name}, price: {self.price}, description: {self.description}"
-#
-#
-# class Cart:
-#     """
-#     Class for cart
-#     Attributes:
-#         customer_name (str): name of customer
-#         products (dict): dictionary of products
-#     """
-#
-#     def __init__(self, customer_name):
-#         self.customer_name = customer_name
-#         self.products = {}
-#
-#     def add_product(self, product, quantity):
-#         """
-#         Add product to cart
-#         :param product: product
-#         :param quantity: quantity of product
-#         :return: None
-#         """
-#
-#         if product not in self.products:
-#             self.products[product] = 0
-#         self.products[product] += quantity
-#
-#     def calculate_total(self):
-#         """
-#         Calculate total price of cart
-#         :return: total price
-#         """
-#
-#         total = 0
-#         for product, quantity in self.products.items():
-#             total += product.price * quantity
-#         return total
-#
-#     def __str__(self):
-#         """
-#         String representation of cart
-#         :return: string
-#         """
-#
-#         cart_str = f"Cart for {self.customer_name}:\n"
-#         for product, quantity in self.products.items():
-#             cart_str += f"{product.name}: {product.description} - {quantity}\n"
-#         cart_str += f"Total: {self.calculate_total()}$"
-#         return cart_str
-#
-#
-# product_1 = Product("Apple", 10, "Green apple")
-# product_2 = Product("Orange", 20, "Orange orange")
-# product_3 = Product("Banana", 30, "Yellow banana")
-# product_4 = Product("Pineapple", 40, "Yellow pineapple")
-# product_5 = Product("Grape", 50, "Purple grape")
-#
-# cart = Cart(input("Enter your name: "))
-#
-# cart.add_product(product_1, 2)
-# cart.add_product(product_2, 3)
-# cart.add_product(product_3, 4)
-# cart.add_product(product_4, 5)
-# cart.add_product(product_5, 6)
-#
-# print(cart)
+class Product:
+    """Class for product
+    Attributes:
+        name (str): name of product
+        price (int): price of product
+        description (str): description of product
+    """
+
+    def __init__(self, name, price, description):
+        self.name = name
+        self.price = price
+        self.description = description
+
+    def __str__(self):
+        return f"Product: {self.name}, price: {self.price}, description: {self.description}"
+
+
+class Cart:
+    """
+    Class for cart
+    Attributes:
+        customer_name (str): name of customer
+        products (dict): dictionary of products
+    """
+
+    def __init__(self, customer_name):
+        self.customer_name = customer_name
+        self.products = {}
+
+    def add_product(self, product, quantity):
+        """
+        Add product to cart
+        :param product: product
+        :param quantity: quantity of product
+        :return: None
+        """
+
+        if product not in self.products:
+            self.products[product] = 0
+        self.products[product] += quantity
+
+    def calculate_total(self):
+        """
+        Calculate total price of cart
+        :return: total price
+        """
+
+        total = 0
+        for product, quantity in self.products.items():
+            total += product.price * quantity
+        return total
+
+    def __str__(self):
+        """
+        String representation of cart
+        :return: string
+        """
+
+        cart_str = f"Cart for {self.customer_name}:\n"
+        for product, quantity in self.products.items():
+            cart_str += f"{product.name}: {product.description} - {quantity}\n"
+        cart_str += f"Total: {self.calculate_total()}$"
+        return cart_str
+
+
+product_1 = Product("Apple", 10, "Green apple")
+product_2 = Product("Orange", 20, "Orange orange")
+product_3 = Product("Banana", 30, "Yellow banana")
+product_4 = Product("Pineapple", 40, "Yellow pineapple")
+product_5 = Product("Grape", 50, "Purple grape")
+
+cart = Cart(input("Enter your name: "))
+
+cart.add_product(product_1, 2)
+cart.add_product(product_2, 3)
+cart.add_product(product_3, 4)
+cart.add_product(product_4, 5)
+cart.add_product(product_5, 6)
+
+print(cart)
 
 
 # Task - 2
